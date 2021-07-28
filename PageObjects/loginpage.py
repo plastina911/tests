0a5ddb286e5e72a5242d4d9880b22b5564052874
Plastina911 <plastina911@gmail.com>
2021-07-28 12:50:31 +0300

    #1 check my frist commit link in task number1

--- PageObjects/loginpage.py
+++ PageObjects/loginpage.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+"""test123test"""
 class Login:
     def __init__(self, driver):
         self.driver = driver
